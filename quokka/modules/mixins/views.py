a73fb2d9449226bd03460789645d384d82d2dc04
kyle roux <jstacoder@gmail.com>
2017-05-31 23:16:49 -0700

    x

--- quokka/modules/mixins/views.py
+++ quokka/modules/mixins/views.py
@@ -1,9 +1,24 @@
 from flask import views
+from quokka.core.templates import render_template
+
+class BaseView(views.MethodView):
+    def get(self, *args, **kwargs):
+        return self.render(*args, **kwargs)
+
+    def render(self, *args, **kwargs):
+        return render_template(*args, **kwargs)    
+    
 
 class ContextMixin(BaseView):    
-    def get_context_data(self, **kwargs)
+    def get_context_data(self, **kwargs):
         context = kwargs
         return context
+
+    def render(self, *args, **kwargs):
+        context = self.get_context_data()
+        kwargs.update(context)
+        return super(ContextMixin, self).render(*args, **kwargs)
+
 
 class TemplateMixin(BaseView):
     template_name = None
@@ -12,5 +27,8 @@ class TemplateMixin(BaseView):
         if self.template_name is None:
             raise ValueError("{} needs a template".format(self.__class__.__name__))
         return self.template_name
+
+    def render(self, *args, **kwargs):
+        return super(TemplateMixin, self).render(self.get_template_name(), *args, **kwargs)
 
     
